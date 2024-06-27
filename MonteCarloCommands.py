bccf98014ec8a16acc5974f36b6c140712e593b4
Joshua Karten <josh.karten@gmail.com>
2024-06-27 10:47:04 -0400

    git config --global user.email "josh.karten@gmail.com" git config --global user.name "Joshua Karten"

--- MonteCarloCommands.py
+++ MonteCarloCommands.py
@@ -7,37 +7,22 @@ def boltzmann_probability(initial, final, beta):
     return np.exp(-beta*(final-initial))
 
 
-def energy(position_x, position_y, array, length_x, length_y, k, two_chains=False):
+def energy(position_x, position_y, array, length_x, length_y, k, two_chains):
     num = array[position_x]
     if two_chains: # needs to be fixed for later. bad imp, 2nd if is redundant, all are missing y edge conditions
-        if position_x==length_x-1:
-            if position_y==length_y-1 or position_y==0:
-                return
-            else:
-                return (-1 ** (~int(num[position_y]))) * k*(4 - (num[slice(0, position_y, position_y-1)].count('0')
-                                                        + array[position_x + 1][position_y].count('0')))
-        if position_x == 0:
-            return (-1 ** int(~num[position_y])) * k* (4 - (num[slice(0, position_y, position_y-1)].count('0')
-                                                        + array[position_x + 1][position_y].count('0')))
-        else:
-            return (-1 ** int(~num[position_y])) *k* (4 - (num[slice(position_y -1, position_y+2,2)].count('0')
-                                                        + array[position_x+1][position_y].count('0') ))
-    elif position_x == length_x-1: #square
-        if position_y == length_y-1:
-            return (-1 ** ~int(num[position_y])) *k* (4 - 2*(num[slice(0, length_y, length_y - 2)].count('0')
-                                                         + array[0][position_y].count('0')
-                                                         + array[position_x - 1][position_y].count('0')))
+        if position_y==length_y-1:
+            return (-1 **(~int(num[position_y]))) * k * (3 - (num[slice(0, position_y, position_y-1)].count('0')
+                                                    + array[1-position_x][position_y].count('0')))
         if position_y==0:
-            return (-1 ** ~int(num[position_y])) * k * (4 - 2 * (num[slice(1, length_y, length_y - 2)].count('0')
-                                                                 + array[0][position_y].count('0')
-                                                                 + array[position_x - 1][position_y].count('0')))
+            return (-1 ** (~int(num[position_y]))) * k * (3 - (num[slice(1, length_y, length_y - 2)].count('0')
+                                                        + array[1 - position_x][position_y].count('0')))
         else:
-           return (-1 ** ~int(num[position_y])) * k*(4 - 2*(num[slice(position_y - 1, position_y + 2, 2)].count('0')
-                                                  + array[0][position_y].count('0')
-                                                  + array[position_x-1][position_y].count('0')))
-    if position_x == 0:
+            return (-1 ** (~int(num[position_y]))) * k*(3 - (num[slice(position_y-1, position_y+2, 2)].count('0')
+                                                    + array[1-position_x][position_y].count('0')))
+
+    elif position_x == 0:
         if position_y == length_y-1:
-            return (-1 ** ~int(num[position_y])) *k* (4 - 2*(num[slice(0, length_y-1, length_y - 2)].count('0')
+            return (-1 ** ~int(num[position_y])) *k* (4 - 2*(num[slice(0, position_y, length_y - 2)].count('0')
                                                          + array[position_x - 1][position_y].count('0')
                                                          + array[length_x-1][position_y].count('0')))
         if position_y==0:
