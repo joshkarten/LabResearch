fd9436c9d093a9ed3217f80ae1140e0bbac9e65d
Joshua Karten <josh.karten@gmail.com>
2024-06-27 12:57:34 -0400

    git config --global user.email "josh.karten@gmail.com" git config --global user.name "Joshua Karten"

--- MonteCarloCommands.py
+++ MonteCarloCommands.py
@@ -4,47 +4,48 @@ from Commands import dec2int
 
 
 def boltzmann_probability(initial, final, beta):
-    return np.exp(-beta*(final-initial))
+    return np.exp(-beta * (final - initial))
 
 
 def energy(position_x, position_y, array, length_x, length_y, k, two_chains):
     num = array[position_x]
-    if two_chains: # needs to be fixed for later. bad imp, 2nd if is redundant, all are missing y edge conditions
-        if position_y==length_y-1:
-            return (-1 **(~int(num[position_y]))) * k * (3 - (num[slice(0, position_y, position_y-1)].count('0')
-                                                    + array[1-position_x][position_y].count('0')))
-        if position_y==0:
+    # likely can save a lot of time here, but time saves are likely correlated to the data stored in lattice
+    if two_chains:
+        if position_y == length_y - 1:
+            return (-1 ** (~int(num[position_y]))) * k * (3 - (num[slice(0, position_y, position_y - 1)].count('0')
+                                                               + array[1 - position_x][position_y].count('0')))
+        if position_y == 0:
             return (-1 ** (~int(num[position_y]))) * k * (3 - (num[slice(1, length_y, length_y - 2)].count('0')
-                                                        + array[1 - position_x][position_y].count('0')))
+                                                               + array[1 - position_x][position_y].count('0')))
         else:
-            return (-1 ** (~int(num[position_y]))) * k*(3 - (num[slice(position_y-1, position_y+2, 2)].count('0')
-                                                    + array[1-position_x][position_y].count('0')))
+            return (-1 ** (~int(num[position_y]))) * k * (3 - (num[slice(position_y - 1, position_y + 2, 2)].count('0')
+                                                               + array[1 - position_x][position_y].count('0')))
 
     elif position_x == 0:
-        if position_y == length_y-1:
-            return (-1 ** ~int(num[position_y])) *k* (4 - 2*(num[slice(0, position_y, length_y - 2)].count('0')
-                                                         + array[position_x - 1][position_y].count('0')
-                                                         + array[length_x-1][position_y].count('0')))
-        if position_y==0:
+        if position_y == length_y - 1:
+            return (-1 ** ~int(num[position_y])) * k * (4 - 2 * (num[slice(0, position_y, length_y - 2)].count('0')
+                                                                 + array[position_x - 1][position_y].count('0')
+                                                                 + array[length_x - 1][position_y].count('0')))
+        if position_y == 0:
             return (-1 ** ~int(num[position_y])) * k * (4 - 2 * (num[slice(1, length_y, length_y - 2)].count('0')
                                                                  + array[position_x + 1][position_y].count('0')
                                                                  + array[length_x - 1][position_y].count('0')))
         else:
-            return (-1 ** ~int(num[position_y])) *k* (4 - 2*(num[slice(position_y - 1, position_y + 2, 2)].count('0')
-                                                         + array[position_x + 1][position_y].count('0')
-                                                         + array[length_x-1][position_y].count('0')))
+            return (-1 ** ~int(num[position_y])) * k * (
+                        4 - 2 * (num[slice(position_y - 1, position_y + 2, 2)].count('0')
+                                 + array[position_x + 1][position_y].count('0')
+                                 + array[length_x - 1][position_y].count('0')))
     else:
-        if position_y == length_y-1:
-            return (-1 ** ~int(num[position_y])) *k* (4 - 2* (num[slice(0, length_y, length_y-2)].count('0')
-                                                         + array[position_x + 1][position_y].count('0')
-                                                         + array[position_x - 1][position_y].count('0')))
+        if position_y == length_y - 1:
+            return (-1 ** ~int(num[position_y])) * k * (4 - 2 * (num[slice(0, length_y, length_y - 2)].count('0')
+                                                                 + array[position_x + 1][position_y].count('0')
+                                                                 + array[position_x - 1][position_y].count('0')))
         if position_y == 0:
             return (-1 ** ~int(num[position_y])) * k * (4 - 2 * (num[slice(1, length_y, length_y - 2)].count('0')
                                                                  + array[position_x + 1][position_y].count('0')
                                                                  + array[position_x - 1][position_y].count('0')))
         else:
-            return (-1 ** ~int(num[position_y])) *k* (4 - 2*(num[slice(position_y - 1, position_y + 2, 2)].count('0')
-                                                         + array[position_x + 1][position_y].count('0')
-                                                         + array[position_x - 1][position_y].count('0')))
-
-
+            return (-1 ** ~int(num[position_y])) * k * (
+                        4 - 2 * (num[slice(position_y - 1, position_y + 2, 2)].count('0')
+                                 + array[position_x + 1][position_y].count('0')
+                                 + array[position_x - 1][position_y].count('0')))
--- ising+control.py
+++ ising+control.py
@@ -5,30 +5,37 @@ from Commands import dec2int, bernoulli, control, order_parameter
 from MonteCarloCommands import energy, boltzmann_probability
 import matplotlib.pyplot as plt
 
+# initialization of parameters
 length = 100
 random.seed(10)
-times = 100
+times = 4
 low_prob = 00
 high_prob = 100 + 1
-Beta = 0
-record = np.zeros([high_prob-low_prob, 1], dtype=float)
+Beta = 1000
+# ferromagnetic/antiferromagnetic
+K = 1
+# update this according to the number of numbers generated
+chains = 2
+#record = np.zeros([high_prob-low_prob, 1], dtype=float)
 record1 = np.zeros([high_prob-low_prob, 1], dtype=float)
 record2 = np.zeros([high_prob-low_prob, 1], dtype=float)
-record3 = np.zeros([high_prob-low_prob, 1], dtype=float)
+#record3 = np.zeros([high_prob-low_prob, 1], dtype=float)
 recordlong = np.zeros((high_prob-low_prob, length), dtype=float)
 for k in range(0, times):
     j = 0
+    # keeps track of loop progress
     print(k)
     for prob in range(low_prob, high_prob, 1):
         #print(prob)
         number1 = random.random()
         number2 = random.random()
-        number3 = random.random()
+        #number3 = random.random()
         #number = tests[k]
         rep1 = dec2int(number1, length)
         rep2 = dec2int(number2, length)
-        rep3 = dec2int(number3, length)
+        #rep3 = dec2int(number3, length)
         #print(k, rep, dec2int(Fraction(2, 3), length), dec2int(Fraction(1, 3), length))
+        # control map
         for i in range((length**2)//2):
             if random.random() > (float(prob) / 100):
                 rep1 = bernoulli(rep1, length)
@@ -38,44 +45,52 @@ for k in range(0, times):
                 rep2 = bernoulli(rep2, length)
             else:
                 rep2 = control(rep2, length)
+                '''
             if random.random() > (float(prob)/100):
                 rep3 = bernoulli(rep3, length)
             else:
                 rep3 = control(rep3, length)
-            lattice = [format(rep1, '0'+str(length)+'b'), format(rep2, '0'+str(length)+'b'),
-                       format(rep3, '0'+str(length)+'b')]
-            for x_pos in range(0, 3):
+                '''
+            # initializing ising lattice. this is most likely a non-optimal step due to the conversions
+            # between binary strings and integers
+            lattice = [format(rep1, '0'+str(length)+'b'), format(rep2, '0'+str(length)+'b')]
+                      # format(rep3, '0'+str(length)+'b')]
+            # iterate through lattice
+            for x_pos in range(0, chains):
                 for y_pos in range(0, length):
-                    E_i = energy(x_pos, y_pos, lattice, 3, length, 1, False)
+                    E_i = energy(x_pos, y_pos, lattice, chains, length, 1, True)
                     old = lattice[x_pos]
                     # noinspection PyTypeChecker
                     lattice[x_pos] = format(int(lattice[x_pos], base=2) ^ (1 << (length - y_pos - 1)), '0' + str(length) + 'b')
-                    E_f = energy(x_pos, y_pos, lattice, 3, length, 1, False)
+                    E_f = energy(x_pos, y_pos, lattice, chains, length, K, True)
                     if random.random() <= boltzmann_probability(E_i, E_f, Beta):
                         pass
                     else:
                         lattice[x_pos] = old
+            # likely can find a way to avoid converting back and forth
             rep1 = int(lattice[0], base=2)
             rep2 = int(lattice[1], base=2)
-            rep3 = int(lattice[2], base=2)
-        record[j] += (order_parameter(rep1, length)+order_parameter(rep2,length)+order_parameter(rep3, length))/(3*times)
+            #rep3 = int(lattice[2], base=2)
+        #record[j] += (order_parameter(rep1, length)+order_parameter(rep2,length)+order_parameter(rep3, length))/(3*times)
         record1[j] += order_parameter(rep1, length)/times
         record2[j] += order_parameter(rep2, length)/times
-        record3[j] += order_parameter(rep3, length)/times
+       # record3[j] += order_parameter(rep3, length)/times
+        # O_perpendicular
         for p in range(length):
-            recordlong[j,p] = order_parameter(int(lattice[0][length-1-p] +lattice[1][length-1-p] +
-                               lattice[2][length-1-p], base=2),3)
+            recordlong[j,p] = order_parameter(int(lattice[0][length-1-p] +lattice[1][length-1-p]
+                                                  # + lattice[2][length-1-p]
+                                                  , base=2),chains)
         j += 1
         #print(j, number, rep, record[j])
 plt.rcParams.update({
     "text.usetex": False,
 })
-print(record[99])
+#print(record[99])
 fig, ax = plt.subplots()
-ax.plot( np.linspace(0,1,high_prob-low_prob), record, marker='o', label="Total Param")
+ax.plot( np.linspace(0,1,high_prob-low_prob), (record1+record2)/2, marker='o', label="Total Param")
 ax.plot( np.linspace(0,1,high_prob-low_prob), record1, marker='o', label= "Chain1 Param")
 ax.plot( np.linspace(0,1,high_prob-low_prob), record2, marker='o', label= "Chain2 Param")
-ax.plot( np.linspace(0,1,high_prob-low_prob), record3, marker='o', label= "Chain3 Param")
+#ax.plot( np.linspace(0,1,high_prob-low_prob), record3, marker='o', label= "Chain3 Param")
 plt.legend(loc='upper left')
 ax.set_xlabel(r'Probability of Selecting the Control Map')
 ax.set_ylabel(r'$ <\hat{O}>$')
@@ -90,7 +105,7 @@ ax.set_title(r'Results of a Simulation with Bitstring length '+str(length))
 #fig.save("200 Length BitString, Full Probability Spectrum")
 #plt.show()
 fig, ax = plt.subplots()
-ax.plot( np.linspace(.3,.6,30), record[30:60], marker='o')
+ax.plot( np.linspace(.3,.6,30), record1[30:60]/2+record2[30:60]/2, marker='o')
 ax.set_xlabel(r'Probability of Selecting the Control Map')
 ax.set_ylabel(r'$ <\hat{O}>$')
 ax.set_title(r'Results of a Simulation with Bitstring length '+str(length))
